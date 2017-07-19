fe00811a5c5eeb5c06f427a37a01e95ff5f8707b
thjenit <thjenxxxno6@gmail.com>
2017-07-19 22:41:56 +0700

    hi

--- app/src/main/java/com/example/qthjen/customlistviewimage/MainActivity.java
+++ app/src/main/java/com/example/qthjen/customlistviewimage/MainActivity.java
@@ -19,11 +19,11 @@ public class MainActivity extends AppCompatActivity {
         lv = (ListView) findViewById(R.id.lv);
         mangMonAn = new ArrayList<MonAn>();
 
-        mangMonAn.add(new MonAn("Vợ yêu 1: ", 0, R.drawable.im5));
-        mangMonAn.add(new MonAn("Vợ yêu 2: ", 1, R.drawable.im1));
-        mangMonAn.add(new MonAn("Vợ yêu 3: ", 2, R.drawable.im2));
-        mangMonAn.add(new MonAn("Vợ yêu 4: ", 3, R.drawable.im3));
-        mangMonAn.add(new MonAn("Vợ yêu 5: ", 4, R.drawable.im4));
+        mangMonAn.add(new MonAn("My wife 1: ", 0, R.drawable.im5));
+        mangMonAn.add(new MonAn("My wife 2: ", 1, R.drawable.im1));
+        mangMonAn.add(new MonAn("My wife 3: ", 2, R.drawable.im2));
+        mangMonAn.add(new MonAn("My wife 4: ", 3, R.drawable.im3));
+        mangMonAn.add(new MonAn("My wife 5: ", 4, R.drawable.im4));
 
         CustomMonAn customMonAn = new CustomMonAn(
                 MainActivity.this,
